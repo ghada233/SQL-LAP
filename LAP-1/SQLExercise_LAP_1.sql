901c5502de1bc09fc2eb81be8a9a23262b471fbc
GhadaAljohani <70041510+ghada233@users.noreply.github.com>
2022-05-21 11:43:49 +0300

    Add files via upload

---
+++ LAP-1/SQLExercise_LAP_1.sql
@@ -0,0 +1,16 @@
+/*ÇäÔÇÁ ŞÇÚÏÉ ÈíÇäÇÊ
+Repeat the same instruction more than once*/
+CREATE  DATABASE  Course
+
+/*ÇäÔÇÁ ÌÏæá ÈŞÇÚÏÉ ÇáÈíÇäÇÊ
+Before creating the table I must specify the required database and 
+select CREATE  TABLE  name_of_table  like that (Name_of_column , data_type)*/
+
+CREATE  TABLE  courses
+(Course_NO  int, Course_Name  varchar(255))
+
+/* drop the table from database 
+íÍĞİ ÇáÌÏæá ßÇãá ÈãÍÊæíÇÊå
+*/
+DROP TABLE courses
+
---
+++ LAP-1/SQLQuery_LAP_1.sql
@@ -0,0 +1,25 @@
+
+/*ÇäÔÇÁ ŞÇÚÏÉ ÈíÇäÇÊ
+Repeat the same instruction more than once*/
+CREATE  DATABASE  LAP_1
+
+/*ÇäÔÇÁ ÌÏæá ÈŞÇÚÏÉ ÇáÈíÇäÇÊ
+Before creating the table I must specify the required database and 
+select CREATE  TABLE  name_of_table  like that (Name_of_column , data_type)*/
+
+CREATE  TABLE  department
+(department_id   int, department_name  varchar(255),  date_of_work   date)
+
+/* drop the table from database 
+íÍĞİ ÇáÌÏæá ßÇãá ÈãÍÊæíÇÊå
+*/
+DROP TABLE department
+
+/* Delete the record  from table
+íÍĞİ ãÍÊæì ÇáÌÏæá æÇáÇÚãÏÉ áÇ ÊÒÇá ãæÌæÏÉ
+*/
+Delete department
+
+/* drop the  database
+ÍĞİ ŞÇÚÏÉ ÇáÈíÇäÇÊ ÈÔßá ßÇãá */
+DROP DATABASE LAP_1
