b1dd445b3844c7987335c8cdf3c90a39daa62d82
GhadaAljohani <70041510+ghada233@users.noreply.github.com>
2022-08-24 19:30:24 +0300

    Update Quiz1_A.sql

--- Quiz_Lap/Quiz1_A.sql
+++ Quiz_Lap/Quiz1_A.sql
@@ -1,3 +1,14 @@
+/*
+Write the correct SQL statements for the following:
+1. Create a database called ‘Quiz_1’
+2. The database has only one table called ‘Students’ which has the following fields:
+a. An integer fieled called Student_no which is the student number and the 
+number should be unique and not null.
+b. A fixed text field called Student_name which is the student name and accepts
+text of exactly 20 characters.
+3. After creating the table, we figured out that the column ‘Student_name’ should not 
+be null. Thus, modify the table to satisfy the condition*/
+
 CREATE DATABASE Quiz_1
 CREATE TABLE Students(
 Student_no int unique NOT NULL ,
